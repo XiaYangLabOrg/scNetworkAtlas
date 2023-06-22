98680e322aeb4a9186c6a252901ae32c894ce13e
Michael Cheng <mcheng7777@ucla.edu>
2023-06-22 14:29:01 -0700

    fixed relative directory paths

--- submission_scripts/submit_run_supercells.sh
+++ submission_scripts/submit_run_supercells.sh
@@ -5,12 +5,14 @@
 tissue_dir="tissue_adata/"
 supercell_dir="supercells"
 
-
+cd ${tissue_dir}
 tissue_celltype_file=tissue_celltype_file.txt
 
-find ./ | grep genes | awk -F'_genes.txt' '{print $1}' > ${tissue_dir}/${tissue_celltype_file}
+find ./ | grep genes | awk -F'_genes.txt' '{print $1}' > ${tissue_celltype_file}
+cd ../
+
 
 num_lines=$(cat ${tissue_dir}/${tissue_celltype_file} | wc -l)
 
 echo $num_lines
-qsub -t 1-${num_lines}:1 ../shell_scripts/run_supercells.sh ${tissue_dir} ${tissue_celltype_file} ${supercell_dir}
+qsub -t 1-${num_lines}:1 shell_scripts/run_supercells.sh ${tissue_dir} ${tissue_celltype_file} ${supercell_dir}

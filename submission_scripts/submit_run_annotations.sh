08824d7e7a828c635457d470bc799a731069ad2c
Michael Cheng <mcheng7777@ucla.edu>
2023-07-11 17:23:15 -0700

    add rerun option, remove higher resolution params

--- submission_scripts/submit_run_annotations.sh
+++ submission_scripts/submit_run_annotations.sh
@@ -4,16 +4,22 @@
 
 
 ##### INPUTS #####
-
+supercell_dir="supercells"
+supercell_file="supercells.txt"
 #Set mode
+rerun="False"
 mode="test" # test or default
 dbs="/u/scratch/m/mikechen/pathway_databases/genesets/human_toy" # human or mouse
 # number of cores to run job
 num_cores=12
 ###################
 # Create folders
-mkdir pathway_annotations
-mkdir pathway_annotations/intermediate_annotations/
+mkdir -p pathway_annotations
+mkdir -p pathway_annotations/intermediate_annotations/
+if [ $rerun = "True" ]
+then
+    mkdir -p temp_files
+fi
 
 modules_dir='gene_memberships/'
 intermediate_dir='./pathway_annotations/intermediate_annotations/'
@@ -22,8 +28,6 @@ convertToHuman="False" # "True" or "False"
 
 
 # Extract all cell types
-supercell_dir="supercells"
-supercell_file="supercells.txt"
 cd ${supercell_dir}
 if [ ! -f ${supercell_file} ]
 then
@@ -34,8 +38,10 @@ if [[ "${mode}" == "test" ]]
 then
     PARAMETERS=('0.01')
     num_supercells=1
+    num_cores=4
 elif [[ "${mode}" == "default" ]]
-    PARAMETERS=('0.002' '0.005' '0.007' '0.01' '0.03' '0.05')
+then
+    PARAMETERS=('0.002' '0.005' '0.007' '0.01')
     num_supercells=$(cat ${supercell_dir}/${supercell_file} | wc -l)
 else
     echo "mode argument must be test or default"
@@ -47,7 +53,36 @@ echo $num_supercells
 
 for i in "${PARAMETERS[@]}"
 do
-    qsub -t 1:${num_supercells} -pe shared ${num_cores} shell_scripts/run_annotations.sh ${supercell_dir}/${supercell_file} ${modules_dir} ${i} ${dbs} ${intermediate_dir} ${convertToHuman} ${num_cores}
-
+    # rerun on supercells without pathway results
+    if [ $rerun = "True" ]
+    then
+        param_subset_file="temp_files/supercells_to_run_${i}.txt"
+        rm -f $param_subset_file # remove if exists
+        while read supercell
+        do
+            if [ ! -f ${intermediate_dir}/${supercell}/${i}_full.txt ]
+            then
+                echo "$supercell" >> ${param_subset_file}
+            else
+                # remove extraneous files
+                ls ${intermediate_dir}/${supercell}/${i}_* | grep -v "full.txt" | xargs rm
+            fi
+        done < ${supercell_dir}/${supercell_file}
+                
+        if [ -f ${param_subset_file} ]
+        then
+            # number of supercells without results
+            num_rerun_supercells=$(cat ${param_subset_file} | wc -l)
+            echo "Rerunning ${i} resolution on ${num_rerun_supercells} celltypes"
+            cat $param_subset_file
+            qsub -t 1:${num_rerun_supercells} -pe shared ${num_cores} shell_scripts/run_annotations.sh ${param_subset_file} ${modules_dir} ${i} ${dbs} ${intermediate_dir} ${convertToHuman} ${num_cores}
+            
+        else
+            echo "No need to rerun ${i} resolution"
+            
+        fi  
+    else
+        qsub -t 1:${num_supercells} -pe shared ${num_cores} shell_scripts/run_annotations.sh ${supercell_dir}/${supercell_file} ${modules_dir} ${i} ${dbs} ${intermediate_dir} ${convertToHuman} ${num_cores}
+    fi
+    echo -e "\n"
 done
-

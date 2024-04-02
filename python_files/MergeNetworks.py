69ea73fb944ad151db4d0792c5bd547afe608164
Michael Cheng <mcheng7777@ucla.edu>
2024-04-02 13:38:02 -0700

    add consensus param for merge scripts, clean up file paths

--- python_files/MergeNetworks.py
+++ python_files/MergeNetworks.py
@@ -1,28 +1,27 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
 if __name__ == "__main__":
 
-# Set number of threads to use
 	import os
 	import warnings
 	warnings.filterwarnings("ignore")
 	import sys
-	sys.path.insert(1,'../../src/')
-	from supercellHelpers import *
-	from buildGRNHelpers import *
-	from MergeNetworksHelpers import *
-	import csv
-
+	import pandas as pd
 	import scanpy as sc
-# In[ ]:
+	from scing import merge
+	import psutil
 
 
 	adata_merged = sc.read(sys.argv[1])
 	all_edge_files = sys.argv[2]
-
-#turn all edge files into one mega file
+	consensus = float(sys.argv[3])
+	outfile = sys.argv[4]
+	remove_cycles = False
+	print(f"number of cpus: {psutil.cpu_count(logical=False)}")
+	outdir = '/'.join(outfile.split('/')[:-1])
+	os.makedirs(outdir, exist_ok=True)
+	# turn all edge files into one mega file
 	files = os.listdir(all_edge_files)
 	files = [f for f in files if ".csv.gz" in f]
 
@@ -30,29 +29,15 @@ if __name__ == "__main__":
 	for i in files:
 		all_edges.append(pd.read_csv(all_edge_files + i))
 
-
-# In[3]:
-
-
-	outfile = sys.argv[1].split("/")[-1].split(".h5ad")[0]
-#outfile
-
-
-# In[13]:
-
-
-	merger = NetworkMerger(adata_merged,
+	merger = merge.NetworkMerger(adata_merged,
                     all_edges,
-                       0.2,
-                    'saved_networks/final_edges',
-                    outfile,
-                    12,
-                    int(2e9),
-                    True)
-
-
-# In[14]:
-
+                    consensus,
+					remove_cycles,
+                    outdir,
+                    outfile.split("/")[-1],
+                    ncore=psutil.cpu_count(),
+                    mem_per_core="auto",
+                    verbose=True)
 
 	merger.preprocess_network_files()
 	merger.remove_reversed_edges()
@@ -60,31 +45,7 @@ if __name__ == "__main__":
 	merger.get_triads()
 	merger.remove_redundant_edges()
 
-
-# In[15]:
-
-
 	all_edges = merger.edge_df.sort_values(by='importance',
                           ascending=False)
 
-
-# In[16]:
-
-
-	all_edges
-	all_edges.to_csv("saved_networks/final_edges/"+outfile+'.csv.gz')
-	#print(all_edges.source)
-	#print(all_edges.target)
-	#print(all_edges.importance)
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+	all_edges.to_csv(outfile)
--- submission_scripts/submit_run_merge.sh
+++ submission_scripts/submit_run_merge.sh
@@ -3,17 +3,26 @@
 # Run from root directory /path/to/Allen_10X/
 
 # make final network directory
-mkdir saved_networks/final_edges
 supercell_dir="supercells"
-supercell_file="supercells.txt"
-cd ${supercell_dir}
+supercell_file="supercells/supercells.txt"
+intermediate_dir="saved_networks/intermediate_data"
+# if testing multiple consensus thresholds
+# consensus_thresholds=(0.2 0.5 0.8 1.0)
+# if running one consensus threshold
+consensus_thresholds=0.5
+out_dir="saved_networks/final_edges"
+
+mkdir -p $out_dir
 if [ ! -f ${supercell_file} ]
 then
-    find ./ | grep h5ad | awk -F'.h5ad' '{print $1}' > ${supercell_file}
+    ls $supercell_dir | grep h5ad | awk -F'.h5ad' '{print $1}' > ${supercell_file}
 fi
-cd ../
 
-num_lines=$(cat ${supercell_dir}/${supercell_file} | wc -l)
+num_lines=$(cat ${supercell_file} | wc -l)
 echo $num_lines
-qsub -t 1-${num_lines}:1 shell_scripts/run_merge.sh $supercell_dir $supercell_file
+
+for consensus in ${consensus_thresholds[@]}
+do
+    qsub -t 1-${num_lines}:1 shell_scripts/run_merge.sh $supercell_dir $supercell_file $consensus $intermediate_dir $out_dir
+done
 

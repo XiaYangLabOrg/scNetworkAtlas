909bf353e71234c278afb0b2f73ed179bc65ee29
Michael Cheng <mcheng7777@ucla.edu>
2024-04-19 14:38:19 -0700

    new pseudobulk scripts

---
+++ submission_scripts/submit_run_supercells_v2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Run from root directory /path/to/Allen_10X/
+tissue_dir="tissue_adata"
+supercell_dir="supercells"
+celltype_col="celltypes"
+sample_col="samples"
+recluster=True
+tissue_celltype_file=tissue_celltype_file.txt
+
+
+if [[ ! -f ${tissue_dir}/${tissue_celltype_file} ]];
+then
+    find $tissue_dir | grep train.*h5ad | awk -F'.h5ad' '{print $1}' | sort > ${tissue_dir}/${tissue_celltype_file}
+fi
+
+num_lines=$(cat ${tissue_dir}/${tissue_celltype_file} | wc -l)
+echo $num_lines
+
+
+qsub -t 1-${num_lines}:1 shell_scripts/run_supercells_v2.sh ${tissue_dir}/${tissue_celltype_file} ${supercell_dir} ${celltype_col} ${sample_col} ${recluster}
